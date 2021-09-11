cff650ac5cfed9f745ae11e30d47ed05cf104e8a
mmadwn <muh.rdwan.u@gmail.com>
2021-09-11 15:41:02 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Switch  } from "react-router-dom";
+import Usecontext from "./Usecontext/Context";
+import Useeffect from "./Useeffect/Effect";
+import Usestate from "./Usestate/State";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <header>
+      <div className="tittle">
+        <p>Kelompok 37</p>
+      </div>
+    <nav>
+      <Link className="text" to="/">
+        useState
+      </Link>
+      <Link className="text navtext2" to="/effect">
+        useEffect
+      </Link>
+      <Link className="text" to="/context">
+        useContext
+      </Link>
+    </nav>
+    </header>
+    <Switch>
+      <Route path="/" exact component={Usestate}/>
+      <Route path="/effect" component={Useeffect}/>
+      <Route path="/context" component={Usecontext}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
---
+++ src/Usestate/State.js
@@ -0,0 +1,56 @@
+import { useState} from "react";
+import "./State.css";
+
+export default function Index() {
+    const [show, setShow] = useState(false);
+    const [count, setCount] = useState(0);
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const countUp = () => setCount(count + 1);
+    const countDown = () => setCount(count - 1);
+    
+    return (
+        <div className="container">
+            {show &&(
+                <>
+                <div className="tittleWrapper">
+                    <p className="tittle">input with useState</p>
+                </div>
+                <div className="tittleWrapper">
+                <div>
+                    <input
+                    placeholder="name"
+                    size="50"
+                    value={user}
+                    onChange={(event) => setUser(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                    placeholder="password"
+                    size="50"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                </div>
+                </>
+            )}
+            <button className="Button" onClick={() => setShow(!show)}>
+                {show ? "hide" : "show"}
+            </button>
+            <div style={{marginBlack: 30}} />
+            <p className="tittle">Counter with useState</p>
+            <p className="countValue">{count}</p>
+            <div className="btnWrapper">
+                <button className="Button" onClick={countUp}>
+                Up
+                </button>
+                <button className="Button" onClick={countDown}>
+                Down
+                </button>
+            </div>
+            </div>
+    );
+}
